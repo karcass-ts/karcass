9e375f2a3f571b5b2b820b8c94190c565d2e4024
Valery <shibaon@gmail.com>
2019-12-18 18:54:27 +0300

    #17 Fix DbService::collectMigrations

--- template/src/Database/Service/DbService.ts
+++ template/src/Database/Service/DbService.ts
@@ -33,21 +33,24 @@ export class DbService extends AbstractService {
         });
     }
 
-    public collectMigrations() {
-        fs.readdirSync('src')
-            .filter((i) => fs.lstatSync(`src/${i}`).isDirectory())
-            .map((i) => `src/${i}/Migrations`)
-            .filter((i) => fs.existsSync(i))
-            .reduce((result, i) => {
-                return [
-                    ...result,
-                    ...fs.readdirSync(i).filter(subi => /\.ts$/.test(subi))
-                        .map(subi => `${i.replace('src/', '')}/${subi.replace('.ts', '.js')}`),
-                ];
-            }, [] as string[])
-            .forEach(async (f) => {
-                this.connection.migrations.push(new (await import(`../../${f}`)).default());
-            });
+    public async collectMigrations() {
+        const bundlesDirs = fs.readdirSync('src').filter(i => fs.lstatSync(path.join('src', i)).isDirectory())
+        const migrations: { filepath: string, timestamp: number }[] = []
+        for (const bundleDir of bundlesDirs) {
+            const migrationsDir = path.join('src', bundleDir, 'Migrations')
+            if (!fs.existsSync(migrationsDir)) {
+                continue
+            }
+            for (const migration of fs.readdirSync(migrationsDir).filter(subi => /\.ts$/.test(subi))) {
+                const timestamp = migration.match(/[0-9]+/)
+                migrations.push({ filepath: path.join(bundleDir, 'Migrations', migration), timestamp: timestamp ? Number(timestamp) : 0 })
+            }
+        }
+        migrations.sort((a, b) => a.timestamp - b.timestamp)
+        for (const migration of migrations) {
+            const jsFileName = path.join('..', '..', migration.filepath.replace('.ts', ''))
+            this.connection.migrations.push(new (await import(jsFileName)).default())
+        }
     }
 
 }
