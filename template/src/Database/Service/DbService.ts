af0207e79e8671fda7c871f62d31755a115922bb
Valery Shibanov <shibaon@gmail.com>
2019-12-19 18:58:16 +0300

    #17 Fix DbService::collectMigrations

--- template/src/Database/Service/DbService.ts
+++ template/src/Database/Service/DbService.ts
@@ -2,6 +2,7 @@ import fs from 'fs';
 import { Application } from '../../Application';
 import { AbstractService } from '../../Base/Service/AbstractService';
 import { Connection, createConnection } from 'typeorm';
+import path from 'path';
 
 export class DbService extends AbstractService {
     public connection!: Connection;
@@ -34,22 +35,22 @@ export class DbService extends AbstractService {
     }
 
     public async collectMigrations() {
-        const bundlesDirs = fs.readdirSync('src').filter(i => fs.lstatSync(path.join('src', i)).isDirectory())
-        const migrations: { filepath: string, timestamp: number }[] = []
+        const bundlesDirs = fs.readdirSync('src').filter(i => fs.lstatSync(path.join('src', i)).isDirectory());
+        const migrations: { filepath: string, timestamp: number }[] = [];
         for (const bundleDir of bundlesDirs) {
-            const migrationsDir = path.join('src', bundleDir, 'Migrations')
+            const migrationsDir = path.join('src', bundleDir, 'Migrations');
             if (!fs.existsSync(migrationsDir)) {
-                continue
+                continue;
             }
             for (const migration of fs.readdirSync(migrationsDir).filter(subi => /\.ts$/.test(subi))) {
-                const timestamp = migration.match(/[0-9]+/)
-                migrations.push({ filepath: path.join(bundleDir, 'Migrations', migration), timestamp: timestamp ? Number(timestamp) : 0 })
+                const timestamp = migration.match(/[0-9]+/);
+                migrations.push({ filepath: path.join(bundleDir, 'Migrations', migration), timestamp: timestamp ? Number(timestamp) : 0 });
             }
         }
-        migrations.sort((a, b) => a.timestamp - b.timestamp)
+        migrations.sort((a, b) => a.timestamp - b.timestamp);
         for (const migration of migrations) {
-            const jsFileName = path.join('..', '..', migration.filepath.replace('.ts', ''))
-            this.connection.migrations.push(new (await import(jsFileName)).default())
+            const jsFileName = path.join('..', '..', migration.filepath.replace('.ts', ''));
+            this.connection.migrations.push(new (await import(jsFileName)).default());
         }
     }
 
