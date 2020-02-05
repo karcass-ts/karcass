02e5114b84a8e9c983da64fabccedf0b592ecf34
Valery Shibanov <shibaon@gmail.com>
2020-02-06 02:31:26 +0300

    It seems ready to release...

--- src/CreateCommand.ts
+++ src/CreateCommand.ts
@@ -3,13 +3,9 @@ import * as ts from 'typescript'
 import fs from 'fs'
 import path from 'path'
 import inquirer from 'inquirer'
-import { MorphyConfig, MorphyService } from './MorphyService'
+import { MorphyService } from './MorphyService'
 import { execSync } from 'child_process'
-import { ConfigParameterType, ConfigParametersResult } from '@karcass/template-reducer'
-
-type ICreateConfig = {
-    directory: string,
-} & MorphyConfig
+import { ConfigParameterType, ConfigParametersResult, TemplateReducerInterface } from '@karcass/template-reducer'
 
 enum SourceType {
     github = 'github',
@@ -24,10 +20,12 @@ export class CreateCommand extends AbstractConsoleCommand {
     }
 
     public async execute() {
+        const originalCwd = process.cwd()
         const config = this.checkBaseConfig()
         let status: 'check'|'copied'|'done' = 'check'
         const cleaner = () => {
             if (status === 'copied') {
+                process.chdir(originalCwd)
                 this.deleteDir(config.destination)
             }
         }
@@ -42,71 +40,34 @@ export class CreateCommand extends AbstractConsoleCommand {
         try {
             if (config.sourceType === SourceType.local) {
                 this.copyDir(config.source, config.destination)
-                status = 'copied'
             }
-            const templateReducerPath = path.join(config.destination, 'TemplateReducer')
-            let sourceCode: string
-            if (fs.existsSync(`${templateReducerPath}.ts`)) {
-                this.write('\nCompiling ')
-                this.write(`${templateReducerPath}.ts`, cc.FgGreen)
-                this.write('... ')
-                sourceCode = fs.readFileSync(`${templateReducerPath}.ts`).toString()
-                sourceCode = ts.transpile(sourceCode, {
-                    target: ts.ScriptTarget.ES2017,
-                    module: ts.ModuleKind.CommonJS,
-                    esModuleInterop: true,
-                })
-                this.writeLn('compiled')
-            } else if (fs.existsSync(`${templateReducerPath}.js`)) {
-                sourceCode = fs.readFileSync(`${templateReducerPath}.js`).toString()
-            } else {
-                throw new Error(`File ${templateReducerPath}.js does not exists in template, unable to continue`)
-            }
-            sourceCode = `let exports = {}; { ${sourceCode} }; exports.TemplateReducer || exports;`
-            console.log(sourceCode)
-            const morphyService = new MorphyService(eval(sourceCode))
+            status = 'copied'
+            const templateReducerConstructor = this.getTemplateReducer(config.destination)
+            const morphyService = new MorphyService(templateReducerConstructor)
 
-            const processConfigParameters = async (configParameters: ConfigParametersResult) => {
-                for (let configParameter of configParameters) {
-                    if (typeof configParameter === 'function') {
-                        const result = await configParameter(morphyService.getConfig())
-                        if (!result) {
-                            continue
-                        }
-                        if (Array.isArray(result)) {
-                            await processConfigParameters(result)
-                            continue
-                        } else {
-                            configParameter = result
-                        }
-                    }
-                    const baseOpts = { name: configParameter.name, message: configParameter.description }
-                    if (configParameter.type === ConfigParameterType.radio || configParameter.type === ConfigParameterType.checkbox) {
-                        const result = await inquirer.prompt({
-                            ...baseOpts,
-                            type: configParameter.type === ConfigParameterType.radio ? 'list' : 'checkbox',
-                            choices: configParameter.choices.map(c => ({
-                                value: c.value,
-                                name: c.description,
-                                checked: c.checked,
-                            })),
-                        })
-                        morphyService.updateConfig(configParameter, result[configParameter.name])
-                    } else {
-                        const result = await inquirer.prompt({
-                            ...baseOpts,
-                            type: configParameter.type as any,
-                            default: configParameter.default,
-                        })
-                        morphyService.updateConfig(configParameter, result[configParameter.name])
-                    }
+            this.writeLn('')
+            await this.processConfigParameters(morphyService)
+
+            for (const dir of await morphyService.getDirectoriesForRemove()) {
+                this.deleteDir(path.join(config.destination, dir))
+            }
+            for (const file of await morphyService.getFilesForRemove()) {
+                fs.unlinkSync(path.join(config.destination, file))
+            }
+            for (const file of this.getDirFilesFlatten(config.destination)) {
+                const innerFilepath = file.slice(config.destination.length + 1)
+                const originalContent = fs.readFileSync(file).toString()
+                const morphedContent = await morphyService.morphyFile(originalContent, innerFilepath)
+                if (originalContent !== morphedContent) {
+                    fs.writeFileSync(file, morphedContent)
                 }
             }
-            this.writeLn('')
-            await processConfigParameters(await morphyService.getConfigParameters())
 
             this.writeLn('\nInstalling packages...')
             this.exec(`cd ${config.destination} && npm install`)
+            process.chdir(config.destination)
+            await morphyService.finish()
+            status = 'done'
         } catch (err) {
             this.writeLn(err.message, cc.FgRed)
             process.exit()
@@ -115,12 +76,9 @@ export class CreateCommand extends AbstractConsoleCommand {
 
     private checkBaseConfig() {
         const config = {
+            name: '',
             destination: process.argv[3],
             source: process.argv[4] ? process.argv[4] : 'https://github.com/karcass-ts/template',
-            name: '',
-            tabSize: 4,
-            semicolon: true,
-            doubleQuotemark: false,
         }
         if (!config.destination) {
             this.write('Missing required argument ', cc.FgRed)
@@ -177,24 +135,84 @@ export class CreateCommand extends AbstractConsoleCommand {
             this.writeLn(destinationFile, cc.FgGreen)
             fs.copyFileSync(sourceFile, destinationFile)
         }
+    }
 
-        /*
-            console.log('Installing npm packages...')
-            await new Promise(resolve => {
-                const pr = exec('cd ' + config.directory + ' ; npm i')
-                pr.on('close', resolve)
-                pr.on('error', (err) => {
-                    console.log(err.message)
-                    resolve()
-                })
-                if (!pr.stdout) {
-                    return this.writeLn('Error: stdout is unavailable')
-                }
-                pr.stdout.on('data', data => console.log(data))
+    private getTemplateReducer(destinationDir: string): new () => TemplateReducerInterface {
+        const templateReducerPath = path.join(destinationDir, 'TemplateReducer')
+        let sourceCode: string
+        if (fs.existsSync(`${templateReducerPath}.ts`)) {
+            this.write('\nCompiling ')
+            this.write(`${templateReducerPath}.ts`, cc.FgGreen)
+            this.write('... ')
+            sourceCode = fs.readFileSync(`${templateReducerPath}.ts`).toString()
+            sourceCode = ts.transpile(sourceCode, {
+                target: ts.ScriptTarget.ES2017,
+                module: ts.ModuleKind.CommonJS,
+                esModuleInterop: true,
             })
-            console.log('Project successfully created.')
-            console.log('Available commands:\nnpm run build\nnpm run watch\nnode index.js')
-        */
+            this.writeLn('compiled')
+        } else if (fs.existsSync(`${templateReducerPath}.js`)) {
+            sourceCode = fs.readFileSync(`${templateReducerPath}.js`).toString()
+        } else {
+            throw new Error(`File ${templateReducerPath}.js does not exists in template, unable to continue`)
+        }
+        sourceCode = `let exports = {}; {\n${sourceCode}\n}; exports.TemplateReducer || exports;`
+        return eval(sourceCode)
+    }
+
+    private async processConfigParameters(morphyService: MorphyService) {
+        const process = async (configParameters: ConfigParametersResult) => {
+            for (let configParameter of configParameters) {
+                if (typeof configParameter === 'function') {
+                    const result = await configParameter(morphyService.getConfig())
+                    if (!result) {
+                        continue
+                    }
+                    if (Array.isArray(result)) {
+                        await process(result)
+                        continue
+                    } else {
+                        configParameter = result
+                    }
+                }
+                const baseOpts = { name: configParameter.name, message: configParameter.description }
+                if (configParameter.type === ConfigParameterType.radio || configParameter.type === ConfigParameterType.checkbox) {
+                    const result = await inquirer.prompt({
+                        ...baseOpts,
+                        type: configParameter.type === ConfigParameterType.radio ? 'list' : 'checkbox',
+                        choices: configParameter.choices.map(c => ({
+                            value: c.value,
+                            name: c.description,
+                            checked: c.checked,
+                        })),
+                    })
+                    morphyService.updateConfig(configParameter, result[configParameter.name])
+                } else {
+                    const result = await inquirer.prompt({
+                        ...baseOpts,
+                        type: configParameter.type as any,
+                        default: configParameter.default,
+                    })
+                    morphyService.updateConfig(configParameter, result[configParameter.name])
+                }
+            }
+        }
+        return process(await morphyService.getConfigParameters())
+    }
+
+    private getDirFilesFlatten(directory: string) {
+        const result: string[] = []
+        for (const part of fs.readdirSync(directory)) {
+            const filename = path.join(directory, part)
+            const stat = fs.statSync(filename)
+            if (stat.isFile()) {
+                result.push(filename)
+                continue
+            } else if (stat.isDirectory()) {
+                result.push(...this.getDirFilesFlatten(filename))
+            }
+        }
+        return result
     }
 
     private exec(command: string) {
--- src/MorphyService.ts
+++ src/MorphyService.ts
@@ -1,148 +1,73 @@
-import fs from 'fs'
-import path from 'path'
 import {
-    BasicInstallationConfig,
     ConfigParameter,
     ConfigParametersResult,
-    ConfigParameterType,
     TemplateReducerInterface,
+    ReplaceFileContentItem,
 } from '@karcass/template-reducer'
-
-export interface LintConfig {
-    tabSize: number
-    semicolon: boolean
-    doubleQuotemark: boolean
-}
-
-export interface MorphyConfig {
-    name: string
-    useDb: 'postgres'
-    useTwing: boolean
-    useLogger: boolean
-    tabSize: number
-    semicolon: boolean
-    doubleQuotemark: boolean
-    port: number
-}
 
 export class MorphyService {
     protected templateReducer: TemplateReducerInterface
-    protected config: BasicInstallationConfig&Record<string, any> = {
-        name: '',
-        defaultInstallation: true,
-        singleQuotemark: true,
-        semicolon: true,
-        tabSize: 4,
-    }
+    protected replacers?: ReplaceFileContentItem[]
 
     public constructor(templateReducerConstructor: new () => TemplateReducerInterface) {
-        console.log(templateReducerConstructor)
         this.templateReducer = new templateReducerConstructor()
     }
 
     public async getConfigParameters(): Promise<ConfigParametersResult> {
-        return [
-            { name: 'type', description: 'Select installation type', type: ConfigParameterType.radio, choices: [
-                { value: 'default', description: 'Default + all features', checked: true },
-                { value: 'select', description: 'Select features' },
-            ] },
-            async config => config.defaultInstallation ? [] : [
-                { name: 'tabSize', description: 'Tab size', type: ConfigParameterType.number, default: 4 },
-                {
-                    name: 'quotemark',
-                    description: 'Use single quitemark (\') instead double (")?',
-                    type: ConfigParameterType.confirm, default: this.config.singleQuotemark,
-                },
-                {
-                    name: 'semicolon',
-                    description: 'Semicolons at end of the lines?',
-                    type: ConfigParameterType.confirm,
-                    default: this.config.semicolon,
-                },
-                ...await this.templateReducer.getConfigParameters(),
-            ],
-        ]
+        return this.templateReducer.getConfigParameters()
+    }
+
+    public getDirectoriesForRemove() {
+        return this.templateReducer.getDirectoriesForRemove()
+    }
+
+    public getFilesForRemove() {
+        return this.templateReducer.getFilesForRemove()
     }
 
     public updateConfig(configParameter: ConfigParameter, result: any) {
-        if (configParameter.name === 'type') {
-            this.config.defaultInstallation = result === 'default'
-            return
-        }
-        this.config[configParameter.name] = result
+        this.templateReducer.setConfig({ [configParameter.name]: result })
     }
 
     public getConfig() {
-        return this.config
+        return this.templateReducer.getConfig()
     }
 
-    public morphyFile(srcFile: string, destFile: string, config: MorphyConfig) {
-        if (destFile.indexOf('.gitignore.template') >= 0) {
-            destFile = destFile.replace('.gitignore.template', '.gitignore')
+    public async morphyFile(content: string, filename: string): Promise<string> {
+        if (filename === 'package.json') {
+            const dependencies = [
+                '@karcass/template-reducer',
+                ...await this.templateReducer.getDependenciesForRemove(),
+            ]
+            const json = JSON.parse(content)
+            for (const dependency of dependencies) {
+                if (dependency in json.dependencies) {
+                    delete json.dependencies[dependency]
+                }
+                if (dependency in json.devDependencies) {
+                    delete json.devDependencies[dependency]
+                }
+            }
+            content = JSON.stringify(json)
         }
-        const basename = path.basename(srcFile)
-        if (basename === 'package.json') {
-            const json = JSON.parse(fs.readFileSync(srcFile).toString())
-            json.name = config.name
-            if (!config.useDb) {
-                delete json.dependencies.typeorm
-                delete json.dependencies.pg
-            }
-            if (!config.useTwing) {
-                delete json.dependencies['@types/luxon']
-                delete json.dependencies.twing
-            }
-            fs.writeFileSync(destFile, JSON.stringify(json, undefined, config.tabSize))
-            return
-        }
-
-        let code = fs.readFileSync(srcFile).toString()
-        if (['package-lock.json'].indexOf(basename) >= 0) {
-            return
-        }
-        if (basename === '.eslintrc.json') {
-            if (config.tabSize !== 4) {
-                code = code.replace('"indent": ["error", 4, { "SwitchCase": 1 }],',
-                    `"indent": ["error", ${config.tabSize}, { "SwitchCase": 1 }],`)
-                code = code.replace('"@typescript-eslint/indent": ["error", 4, { "SwitchCase": 1 }],',
-                    `"@typescript-eslint/indent": ["error", ${config.tabSize}, { "SwitchCase": 1 }],`)
-            }
-            if (!config.semicolon) {
-                code = code.replace('"semi": ["error", "always"],', '"semi": ["error", "never"],')
-            }
-            if (config.doubleQuotemark) {
-                code = code.replace('"quotes": ["error", "single"],', '"quotes": ["error", "double"],')
+        for (const replacer of await this.getReplacers()) {
+            if (replacer.filename instanceof RegExp && replacer.filename.test(filename) || replacer.filename === filename) {
+                content = await replacer.replacer(content, filename)
             }
         }
 
-        if (config.doubleQuotemark) {
-            code = code.replace('"quotemark": { "options": "single" },', '"quotemark": { "options": "double" },')
-        }
-        if (['config.js', 'config.js.dist'].indexOf(basename) >= 0) {
-            code = code.replace('1000000000', `${config.port}`)
-        }
-        if (['.ts', '.js'].indexOf(path.extname(srcFile).toLowerCase()) >= 0) {
-            code = this.lint(code, config)
-        }
-        fs.writeFileSync(destFile, code)
-        if (code.indexOf('#!/usr/bin/env node') >= 0) {
-            fs.chmodSync(destFile, '0755')
-        }
+        return content
     }
 
-    public lint(code: string, config: LintConfig) {
-        if (config.tabSize !== 4) {
-            let str = ''
-            for (str = ''; str.length < config.tabSize; str += ' ') {/**/}
-            code = code.replace(/ {4}/g, str)
+    public finish() {
+        return this.templateReducer.finish()
+    }
+
+    protected async getReplacers() {
+        if (this.replacers) {
+            return this.replacers
         }
-        if (!config.semicolon) {
-            code = code.replace(/;/g, '')
-        }
-        if (config.doubleQuotemark) {
-            code = code.replace(/'/g, '"')
-        }
-        return code
+        return this.replacers = await this.templateReducer.getFilesContentReplacers()
     }
 
 }
